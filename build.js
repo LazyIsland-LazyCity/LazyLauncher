5c7141ad0412e8b6224f4b81fdd51a6a3b6207f1
ZaptoInc <38966434+ZaptoInc@users.noreply.github.com>
2021-04-08 22:19:55 +0200

    fix maj pour le lancement

--- build.js
+++ build.js
@@ -19,7 +19,7 @@ builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
         appId: 'dymensialauncher',
-        productName: 'Dymensia Launcher',
+        productName: 'DymensiaLauncher',
         artifactName: '${productName}-setup-${version}.${ext}',
         copyright: 'Copyright © 2018-2020 Daniel Scalzi',
         directories: {
