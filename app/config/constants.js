d6c8cdb0140c48385314907ac2a061a2e3b5019f
Dorey Grégoire <43789150+Lenoob50@users.noreply.github.com>
2025-10-17 19:30:54 +0200

    Modification

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,5 +1,5 @@
-const APP_DATA_NAME = 'DymensiaLauncher'
-const LIVE_DISTRIBUTION_URL = 'https://launcher.dymensia.fr/files/distribution.json'
-const DEV_DISTRIBUTION_URL = 'https://launcher.dymensia.fr/files/distribution.json'
+const APP_DATA_NAME = 'LazyIsland'
+const LIVE_DISTRIBUTION_URL = 'https://dl.lazycity.fr/downloads/distribution.json'
+const DEV_DISTRIBUTION_URL = 'https://dl.lazycity.fr/downloads/distribution.json'
 
 module.exports = { APP_DATA_NAME, LIVE_DISTRIBUTION_URL, DEV_DISTRIBUTION_URL }
